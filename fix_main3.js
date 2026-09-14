a5bca15a4017251f2c63e5b4d67b37500e288d91
Yakfal <Yakfal@users.noreply.github.com>
2026-09-14 01:04:55 -0400

    chore: remove development scratch files and stale builder exclusions

--- fix_main3.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const content = fs.readFileSync('electron/main.js', 'utf8');
-const lines = content.split('\n');
-// Remove the extra }); at line 796 (0-indexed: 795)
-const fixedLines = lines.filter((line, index) => index !== 795);
-fs.writeFileSync('electron/main.js', fixedLines.join('\n'));
-console.log('Fixed');
